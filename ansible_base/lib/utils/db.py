c7b6580e508e863856073ad45572b88183ad2a53
Alan Rominger <arominge@redhat.com>
2024-08-07 15:45:17 -0400

    fix test written badly

--- ansible_base/lib/utils/db.py
+++ ansible_base/lib/utils/db.py
@@ -2,7 +2,13 @@ from django.db import connection
 from django.db.migrations.executor import MigrationExecutor
 
 
-def migrations_are_complete():
+def migrations_are_complete() -> bool:
+    """Returns a booling telling you if manage.py migrate has been run to completion
+
+    Note that this is a little expensive, like up to 20 database queries
+    and lots of imports.
+    Not suitable to run as part of a request, but expected to be okay
+    in a management command or post_migrate signals"""
     executor = MigrationExecutor(connection)
     plan = executor.migration_plan(executor.loader.graph.leaf_nodes())
     return not bool(plan)
--- test_app/tests/lib/utils/test_db.py
+++ test_app/tests/lib/utils/test_db.py
@@ -6,4 +6,4 @@ from ansible_base.lib.utils.db import migrations_are_complete
 @pytest.mark.django_db
 def test_migrations_are_complete():
     "If you are running tests, migrations (test database) should be complete"
-    assert migrations_are_complete
+    assert migrations_are_complete()
